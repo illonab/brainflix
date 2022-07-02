ecb174da4f2ae1380e251a3fb625f709cecbc114
Ilona Beshchuk <lona.krechko@gmail.com>
2022-07-02 17:16:59 +0100

    Created app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import "./App.scss";
 import Header from "./components/Header/Header";
+import MainVideo from "./components/MainVideo/MainVideo";
+import VideoDetail from "./components/VideoDetails/VideoDetails";
+import VideoList from "./components/VideoList/VideoList";
+
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <MainVideo />
+        <div className="main__wrapper">
+          <VideoDetail></VideoDetail>
+          <VideoList />
+        </div>
+      </main>
     </>
   );
 }
