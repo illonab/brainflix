d5bcf2aa8cde807f6eadc485fc4a663d9cd90550
Ilona <ilona.b.mail@gmail.com>
2022-07-09 20:42:27 +0100

    Cleanup

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,4 +1,5 @@
 import "./Video.scss";
+import { Link } from "react-router-dom";
 
 function Video(props) {
   const clickHandler = () => {
@@ -7,7 +8,7 @@ function Video(props) {
 
   return (
     <article className="video" onClick={clickHandler}>
-      <img className="video__image" src={props.video.image} alt="" />
+      <img className="video__image" src={props.video.image} alt="main video" />
       <div className="video__content">
         <h3 className="video__title">{props.video.title}</h3>
         <p className="video__author">{props.video.channel}</p>
