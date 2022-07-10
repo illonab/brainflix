83a4e9aa8963754f785cf5044083b6179078f9a4
Ilona <ilona.b.mail@gmail.com>
2022-07-10 14:07:01 +0100

    Fetching main video details

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,55 @@
 import MainVideo from "../../components/MainVideo/MainVideo";
 import VideoDetail from "../../components/VideoDetails/VideoDetails";
 import VideoList from "../../components/VideoList/VideoList";
+import React, { Component } from "react";
+import axios from "axios";
+import { API_KEY, API_URL } from "../../config";
 
-function Home(props) {
-  console.log(props.state);
-  return (
-    <main className="main">
-      <MainVideo video={props.state.mainVideo} />
-      <div className="main__wrapper">
-        <VideoDetail details={props.state.mainVideo} />
-        <VideoList
-          list={props.state.videosData}
-          mainVideo={props.state.mainVideo}
-          changeActiveVideo={props.state.changeActiveVideo}
-        />
-      </div>
-    </main>
-  );
+class Home extends Component {
+  state = {
+    mainVideo: null,
+  };
+
+  async fetchMainVideo() {
+    if (
+      this.state.mainVideo === null ||
+      this.state.mainVideo.id !== this.props.videoId
+    ) {
+      try {
+        const response = await axios.get(
+          `${API_URL}/videos/${this.props.videoId}?api_key=${API_KEY}`
+        );
+        this.setState({ mainVideo: response.data });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  async componentDidUpdate() {
+    this.fetchMainVideo();
+  }
+
+  async componentDidMount() {
+    this.fetchMainVideo();
+  }
+
+  render() {
+    if (this.state.mainVideo === null) {
+      return null;
+    }
+    return (
+      <main className="main">
+        <MainVideo video={this.state.mainVideo} />
+        <div className="main__wrapper">
+          <VideoDetail details={this.state.mainVideo} />
+          <VideoList
+            list={this.props.videos}
+            mainVideo={this.state.mainVideo}
+          />
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Home;
