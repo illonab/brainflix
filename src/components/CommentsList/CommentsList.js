4fc682bda40a31f9a2139db7cc76045c75bcdd55
Ilona <ilona.b.mail@gmail.com>
2022-07-17 19:29:36 +0100

    Delete console.log

--- src/components/CommentsList/CommentsList.js
+++ src/components/CommentsList/CommentsList.js
@@ -2,7 +2,6 @@ import "./CommentsList.scss";
 import Comment from "../Comment/Comment";
 
 function CommentsList(props) {
-  console.log(props.list);
   const sortedList = [...props.list];
   sortedList.sort((a, b) => {
     return b.timestamp - a.timestamp;
