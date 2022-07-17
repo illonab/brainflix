9fbe39ede819ec00ca3c9f7558eb057c0829d3c8
Ilona <ilona.b.mail@gmail.com>
2022-07-17 17:11:56 +0100

    add upload video functionality

--- src/App.js
+++ src/App.js
@@ -13,10 +13,14 @@ class App extends Component {
     videosData: [],
   };
 
+  getData = async () => {
+    const response = await axios.get(`${API_URL}/videos?api_key=${API_KEY}`);
+    this.setState({ videosData: response.data });
+  };
+
   async componentDidMount() {
     try {
-      const response = await axios.get(`${API_URL}/videos?api_key=${API_KEY}`);
-      this.setState({ videosData: response.data });
+      this.getData();
     } catch (err) {
       console.log(err);
     }
@@ -43,7 +47,12 @@ class App extends Component {
               );
             }}
           />
-          <Route path="/upload" component={Upload}></Route>
+          <Route
+            path="/upload"
+            render={(routeProps) => {
+              return <Upload getData={this.getData} {...routeProps} />;
+            }}
+          ></Route>
           <Route
             path="/videos/:videoId"
             render={(routeProps) => {
